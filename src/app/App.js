702de9cf5d6a80a42293b755ab2fb082dd10bbfa
Jordi Bolo <152198990+jordybc1503@users.noreply.github.com>
2024-06-10 18:13:36 -0500

    final project

---
+++ src/app/App.js
@@ -0,0 +1,45 @@
+import React from "react";
+import About from "../components/About";
+import SignUp from "../components/SignUp";
+import Articles from "../components/Articles";
+import Article from "../components/Article";
+import Categories from "../components/Categories";
+import Category from "../components/Category";
+import Author from "../components/Author";
+import Profile from "../components/Profile";
+import EditProfileForm from "../components/EditProfileForm";
+import Root from "../components/Root";
+
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+
+import "./App.css";
+
+const router = 
+createBrowserRouter(createRoutesFromElements(
+   <Route path="/" element={ <Root/> }>
+    <Route path="/about" element={ <About/>}></Route>
+    <Route path="/sign-up" element={ <SignUp/>}></Route>
+    <Route path="/articles" element={ <Articles/>}></Route>
+    <Route path="/articles/:title" element={ <Article/>}></Route>
+    <Route path="/authors/:name" element={ <Author/>}></Route>
+    <Route path="/categories" element={ <Categories/>}>
+      <Route path=":name" element={<Category/>}></Route>
+    </Route>  
+    <Route path="profile" element={ <Profile/>}>
+      <Route path="edit" element= {<EditProfileForm/>}></Route>
+    </Route>
+  </Route>
+    )
+ 
+)
+
+function App() {
+  return (
+    <>
+      { /* Replace below and add Router Provider*/}
+      <RouterProvider router = { router}/>
+    </>
+  );
+}
+
+export default App;
